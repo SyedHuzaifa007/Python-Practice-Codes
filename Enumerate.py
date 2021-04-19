71a75a36f79c2a2cd87200a4df86d572399003ab
Syed Huzaifa <68459184+SyedHuzaifa007@users.noreply.github.com>
2021-04-19 22:26:26 +0500

    Update Enumerate.py

--- Enumerate.py
+++ Enumerate.py
@@ -12,3 +12,13 @@ print(list(enumerate(l1)))
 
 # Changing Start Index To 2 From 0
 print(list(enumerate(s1,2)))
+
+# Using Enumerate Function In Loops
+
+# Printing The Tuples In The Objects Directly
+for ele in enumerate(l1):
+    print(ele)
+
+# Changing Index And Printing Searately
+for count, ele in enumerate(l1, 100):
+    print(count, ele)
