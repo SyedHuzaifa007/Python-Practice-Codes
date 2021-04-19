5f5ab352e5c0be97bf91f33d69f09b923aeb2cf5
Syed Huzaifa <68459184+SyedHuzaifa007@users.noreply.github.com>
2021-04-19 23:10:34 +0500

    Update Logging.py

--- Logging.py
+++ Logging.py
@@ -11,4 +11,12 @@ def main():
     mode = 'r'
 
     # Example Logging Calls (Insert Into Your Program)
-    hostname = 'www.python.org'
+    logging.critical('Host %s is unknown', hostname)
+    logging.error("Couldn't find %r", item)
+    logging.warning('Feature is deprecated')
+    logging.info('Opening file %r, mode = %r', filename, mode)
+    logging.debug('Got here')
+    
+if __name__ == '__main__':
+    main()
+
