19eb789672525da73969edf4b513ba3ca8506226
Syed Huzaifa <huzaifanau123@gmail.com>
2021-04-20 22:47:19 +0500

    Update List Comprehension

--- List Comprehension.py
+++ List Comprehension.py
@@ -6,3 +6,9 @@ for x in fruits:
         newlist.append(x)
 
 print(newlist)
+
+# Now With The List Comprehension
+newlist = [x for x in fruits if "a" in x]
+print(newlist)
+
+# Syntax: variable = [expression for item in iterable if condition == True]
