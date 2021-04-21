f1d93634e0feff6b31685a814ac5478173554e3e
Syed Huzaifa <huzaifanau123@gmail.com>
2021-04-21 22:58:26 +0500

    Update Classes.py

--- Classes.py
+++ Classes.py
@@ -2,4 +2,11 @@ class Person:
     def __init__(self, fname, lname):
         self.firstname = fname
         self.lastname = lname
-        
+
+    def printname(self):
+        print(self.firstname, self.lastname)
+
+# Use The Person Class To Create An Object, And Then Execute The PrintName Method:
+
+x = Person("Leonardo", "Di Vinci")
+x.printname()
