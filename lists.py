e3f683a7c03d23986eedcf49bf150c8d5a138d8a
SyedHuzaifa007 <huzaifanau123@gmail.com>
2021-08-04 23:35:17 +0500

    Updated File

--- lists.py
+++ lists.py
@@ -1,7 +1,18 @@
+from typing import List
+
+
 fruits = ["Apple", "Mangoes", "Bananas", "Pineapples", "Oranges"]
 cities = ["Lahore", "Delhi", "Italy", "New York", "London"]
+
+# Slicing List-1
 print(fruits[0])
 print(fruits[1])
 print(fruits[2])
 print(fruits[3])
 print(fruits[4])
+# Reverse Slicing List-1
+print(fruits[-1])
+print(fruits[-2])
+print(fruits[-3])
+print(fruits[-4])
+print(fruits[-5])
