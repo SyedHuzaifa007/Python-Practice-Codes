1573377398506a01285b908c45ca64b1af57be73
SyedHuzaifa007 <huzaifanau123@gmail.com>
2021-08-04 23:31:10 +0500

    Updated File

--- lists.py
+++ lists.py
@@ -1,2 +1,7 @@
 fruits = ["Apple", "Mangoes", "Bananas", "Pineapples", "Oranges"]
 cities = ["Lahore", "Delhi", "Italy", "New York", "London"]
+print(fruits[0])
+print(fruits[1])
+print(fruits[2])
+print(fruits[3])
+print(fruits[4])
