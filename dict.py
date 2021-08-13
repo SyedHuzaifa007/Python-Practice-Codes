da14817c14da761c883d6227d7ccaefc40c36e43
SyedHuzaifa007 <huzaifanau123@gmail.com>
2021-08-13 22:34:17 +0500

    Updated dict.py

--- dict.py
+++ dict.py
@@ -7,5 +7,11 @@ user_info = {
      "address":"somewhere on the Earth"
 }
 
+# Accessing The Values From The Keys 
 print(user_info["name"])
 print(user_info["phone"])
+
+# Updating The Values of Existing Keys
+user_info["name"] = "John Wick"
+print(user_info["name"])
+print(user_info)
