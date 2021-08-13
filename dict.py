b9684bdf41617e4fe047db12af0fc8128b154e79
SyedHuzaifa007 <huzaifanau123@gmail.com>
2021-08-13 22:39:47 +0500

    Updated dict.py

--- dict.py
+++ dict.py
@@ -15,3 +15,21 @@ print(user_info["phone"])
 user_info["name"] = "John Wick"
 print(user_info["name"])
 print(user_info)
+user_info["phone"] = 67786867856
+print(user_info)
+
+# Deleting The Value Pair From A Dictionary
+del user_info["address"]
+print(user_info)
+
+# Adding A Key Value Pair In The Exsisting Dictionary
+user_info["DOB"] = "01-01-1000"
+print(user_info)
+
+
+
+
+
+
+
+
