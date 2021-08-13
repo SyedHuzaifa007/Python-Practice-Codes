9550d137260b21c26609d6612ed27ad820a22017
SyedHuzaifa007 <huzaifanau123@gmail.com>
2021-08-13 23:25:01 +0500

    Updated petrol_pump_prob.py

--- petrol_pump_prob.py
+++ petrol_pump_prob.py
@@ -1,5 +1,8 @@
 import datetime
+import time
+
 # User Information
+
 user_name = input("Enter Your Name: ")
 car_name = input("Enter Your Car Name: ")
 car_model = input("Enter Your Car Model: ")
@@ -8,7 +11,16 @@ biometric = input("Are You Sure To Scan Your Fingerprint For Biometric (Y/N): ")
 if biometric == "Y" or "y":
     print("Place Your Finger On Fingerprint Scanner")
     print("Scannig.....")
+    time.sleep(5)
     print("Your ID Is Confirmed, Go Ahead And Refule Your Car")
 elif biometric != "Y" or "y":
     print("Sorry! We Can't Refule Your Car")
-print("Hello")
+    quit
+
+# Fuling Machine 
+
+fuel_price = 100
+print(f"Price of Perol is ${fuel_price}/L Today")
+quantity = int(input("Enter The Quantity Of Petrol You Want To Refule (In Litres): "))
+print("Please Wait While We Refule Your Car.....")
+time.sleep(10)
