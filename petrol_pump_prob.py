61d951a5e8f14541a2b88edd7b9ab3c426a15a4e
SyedHuzaifa007 <huzaifanau123@gmail.com>
2021-08-13 23:32:03 +0500

    Updated petrol_pump_prob.py

--- petrol_pump_prob.py
+++ petrol_pump_prob.py
@@ -1,5 +1,7 @@
 import datetime
+from os import name
 import time
+import random 
 
 # User Information
 
@@ -24,3 +26,35 @@ print(f"Price of Perol is ${fuel_price}/L Today")
 quantity = int(input("Enter The Quantity Of Petrol You Want To Refule (In Litres): "))
 print("Please Wait While We Refule Your Car.....")
 time.sleep(10)
+print("Your Car Is Refule, Please Take This Ticket Number And Head To The Next Machine For Paying")
+ticket_number = random.randint(1, 100000)
+print(f"Ticket Number: {ticket_number}")
+
+# Cash And Reciept Machine
+
+print("Welcome To Pak Petrol Pump, Hope You Enjoying The Day!")
+ticket_number_verify = int(input("Enter Your Car Ticket Number Issued To You: "))
+if ticket_number_verify == ticket_number:
+    print("Your Ticket Number Is Confirmed, Please Pay The Total Amount of: ${total_amount})")
+    time.sleep(5)
+    confirmed = input("Press 'P' On The Screen After You Pay: ")
+    time.sleep(3)
+    if confirmed == "P":
+        print_reciept = (input("Your Amount Has Been Paid Successfully, Please Press '1' On The Screen To Print Reciept"))
+        if print_reciept == "1":
+            print("Your Reciept Will Be Printed In 5 Seconds")
+            time.sleep(5)
+            print("Reciept Printed")
+            reciept = print(f"Customer Name: {name}__________Car Name: {car_name}\n"
+                            f"Car Model: {car_model}__________Car Number: {car_number}\n"
+                            f"Ticket Number: {ticket_number}\n"
+                            f"Price Of Petrol: ${fuel_price}\L\n"
+                            f"Quantity Of Petrol: {quantity} Litres\n"
+                            f"Total Amount: ${quantity * fuel_price}\n"
+                            f"Payment Method: Cash\n"
+                            f"Payment Date: {datetime.datetime.now()}\n"
+                            "Thanks For Visiting Pak Petrol Pump. Enjoy Your Day!")
+            print(reciept)
+            print("Thank You For UsingPak Petrol Pump")
+            print("Have A Nice Day")
+            quit
