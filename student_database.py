be55116e54a5d64570584cd92d60d5f661f47bb3
SyedHuzaifa007 <huzaifanau123@gmail.com>
2021-08-16 23:39:09 +0500

    Updated student_database.py

---
+++ student_database.py
@@ -0,0 +1,7 @@
+# Taking Student Information 
+
+name = input("Enter Student Name: ")
+age = input("Enter Student Age: ")
+roll_no = input("Enter Student Roll Number: ")
+branch = input("Enter Student Branch: ")
+year = input("Enter Student Year: ")
