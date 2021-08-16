d1b0c530867926f8f7a394e8f25ae7cf96e03861
SyedHuzaifa007 <huzaifanau123@gmail.com>
2021-08-16 23:49:51 +0500

    Updated student_database.py

--- student_database.py
+++ student_database.py
@@ -7,3 +7,7 @@ age = input("Enter Student Age: ")
 roll_no = input("Enter Student Roll Number: ")
 branch = input("Enter Student Branch: ")
 year = input("Enter Student Year: ")
+
+with open("student_database.txt", "a") as f:
+    f.write("\n" + str(serail_no) + " " + name + " " + age + " " + roll_no + " " + branch + " " + year)
+    
